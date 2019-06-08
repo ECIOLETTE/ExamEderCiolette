db5efe20ee41a9b2172159ba879281b1bdaf1f05
college@bheshop.com.br <college@bheshop.com.br>
2019-06-07 20:26:49 -0700

    Exam Final Eder Ciolette

---
+++ ExamEder/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace ExamEder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ------------------- NUMBER 1 -------------------
+
+            //DateTime localDate = DateTime.Now;
+            //Console.WriteLine("Eder Ademir Ciolette " + localDate);
+            //Console.ReadKey();
+
+
+            // ------------------- NUMBER 2 -------------------
+
+            //    string texto;
+
+            //    Console.WriteLine("Enter a Phrase: ");
+            //    texto = Console.ReadLine();
+            //    Console.WriteLine(qtdLetter(texto).ToString() + " - Letters");
+            //    Console.ReadLine();
+
+            //static int qtdLetter(string texto)
+            //{
+            //    int qtd = 0;
+            //    foreach (char letra in texto)
+            //    {
+            //        if (char.IsLetter(letra))
+            //        {
+            //            qtd++;
+            //        }
+            //    }
+            //    return qtd;
+            //}
+
+
+            // ------------------- NUMBER 3 -------------------
+
+            //int num;
+            //int number;
+
+            //Console.WriteLine("Enter a number: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //number = num / 2;
+
+            //if ((number % 2) == 0)
+            //{
+            //    Console.WriteLine("ODD");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("EVEN");
+            //}
+            //Console.ReadKey();
+
+
+            // ------------------- NUMBER 4 -------------------
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+
+            // ------------------- NUMBER 5 -------------------
+
+            //int number1;
+            //int number2;
+            //int result;
+            //bool b = true;
+
+            //Console.Write("Enter Number1: ");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter Number2: ");
+            //number2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("=============================");
+
+            //Console.WriteLine("What is the result?");
+            //result = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("=============================");
+
+            //int number3 = number1 * number2;
+            //Console.WriteLine("{0} x {1} = {2}", number1, number2, number3);
+
+            //if (number3 == result)
+            //{
+            //    Console.WriteLine(b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+
+            // ------------------- NUMBER 6 -------------------
+
+            //DateTime dt = new DateTime();
+
+            //Console.WriteLine("enter the Date:(mm/dd/yyyy)");
+            //dt = Convert.ToDateTime(Console.ReadLine());
+
+            //Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
+            //Console.ReadLine();
+
+
+            // ------------------- NUMBER 7 -------------------
+
+            //int num;
+
+            //Console.WriteLine("Enter a Number (Positive or Negative): ");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            //if (num >= 0)
+            //{
+            //    Console.WriteLine(" Number Positive ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Number Negative ");
+            //}
+
+            //Console.ReadKey();
+
+
+            // ------------------- NUMBER 8 -------------------
+
+            //string text;
+            //string textUpper;
+
+            //Console.WriteLine("Enter text: ");
+            //text = Console.ReadLine();
+
+            //textUpper = text.ToUpper();
+            //Console.WriteLine(textUpper);
+
+            //Console.ReadLine();
+
+
+            // ------------------- NUMBER 9 -------------------
+
+
+            //    int[] array1 = new int[6];
+
+            //    Console.WriteLine("Entre six numbers: ");
+            //    for (int x = 0; x < 6; x++)
+            //    {
+            //        array1[x] = int.Parse(Console.ReadLine());
+            //    }
+            //    ordena(array1);
+            //}
+
+            //static void ordena(int[] array1)
+            //{
+            //    Array.Sort(array1);
+            //    Console.WriteLine("Ascending Order:");
+
+            //    for (int x = 0; x < 6; x++)
+            //    {
+            //        Console.WriteLine(array1[x]);
+            //    }
+            //    Console.ReadKey();
+
+
+            // ------------------- NUMBER 10 ------------------
+
+            //Console.Write("Enter Number: ");
+            //int num = Int32.Parse(Console.ReadLine());
+            //int factorial = 1;
+
+            //if (num != 0)
+            //{
+            //    for (int i = 1; i <= num; i++) factorial = factorial * i;
+            //}
+
+            //Console.WriteLine("Factorial: " + num + " = " + factorial);
+        }
+    }
+}
+
